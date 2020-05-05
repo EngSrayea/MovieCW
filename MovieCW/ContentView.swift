4960ecb738fa238f2673437babfd013eddc4439e
EngSrayea <eng.nsrayea@gmail.com>
2020-05-05 23:14:47 +0300

    Initial Commit

---
+++ MovieCW/ContentView.swift
@@ -0,0 +1,21 @@
+my//
+//  ContentView.swift
+//  MovieCW
+//
+//  Created by Eng. Norii on 5/5/20.
+//  Copyright © 2020 EngSrayea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
